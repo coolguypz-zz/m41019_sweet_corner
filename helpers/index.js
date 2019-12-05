4abd71238798f596e7a3d8ddf824a47f9ef18b1f
Peterzhang123 <peterzzq1986@gmail.com>
2019-12-05 19:38:38 +0000

    Setup DB and completed get all products endpoint

---
+++ helpers/index.js
@@ -0,0 +1,10 @@
+module.exports.buildUrl = (req,type,file) =>{
+  
+  const protocol  = req.protocol;
+
+  const host = req.get('host');
+
+  const url = `${protocol}://${host}/images/${type}/${file}`;
+
+  return url;
+}
