f3337247775178c3ee411d3fece6c330580362fa
Peterzhang123 <peterzzq1986@gmail.com>
2019-12-18 17:05:14 +0000

    update on Dec 17 - with_cart token

--- client/webpack.dev.js
+++ client/webpack.dev.js
@@ -1,18 +1,16 @@
-const webpack = require('webpack');
-const merge = require('webpack-merge');
-const common = require('./webpack.common');
-const PORT = process.env.PORT || 4001;
+const webpack = require("webpack");
+const merge = require("webpack-merge");
+const common = require("./webpack.common");
+const PORT = process.env.PORT || 3000;
 
 module.exports = merge(common, {
-    entry: [
-        'core-js/stable',
-        'regenerator-runtime/runtime',
-        'webpack-dev-server/client?http://localhost:' + PORT,
-        './index.js'
-    ],
-    devtool: 'inline-source-map',
-    mode: 'development',
-    plugins: [
-        new webpack.NamedModulesPlugin()
-    ]
+   entry: [
+      "core-js/stable",
+      "regenerator-runtime/runtime",
+      "webpack-dev-server/client?http://localhost:" + PORT,
+      "./index.js",
+   ],
+   devtool: "inline-source-map",
+   mode: "development",
+   plugins: [new webpack.NamedModulesPlugin()],
 });
